f50b175f85197cda8498d3aeb3e02303ad7d7ebb
rashiya795 <rashiya262@gmail.com>
2025-07-08 23:07:15 +0530

    deletedunwanted lines

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import React from 'react'
-import Homepage from './Components/Homepage'
+
 import Navbar from './Components/Navbar'
 import Footer from './Components/Footer'
 import { Outlet } from 'react-router-dom'
 
-import bookStore from './Components/BookStore'
 
 export default function App() {
   return (
